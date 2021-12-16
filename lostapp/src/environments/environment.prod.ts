f29d4935dce62413882b614e4c55492b7717ee04
ManuelExposito <manuelexposito4d@gmail.com>
2021-12-16 08:51:13 +0100

    First commit

---
+++ lostapp/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'lostapp-2745c',
+    appId: '1:503341816357:web:ed609c20f1d88392c3bdcc',
+    storageBucket: 'lostapp-2745c.appspot.com',
+    apiKey: 'AIzaSyCUKrt60o_JgDXLUeCKylLf7ijzZE2kCMU',
+    authDomain: 'lostapp-2745c.firebaseapp.com',
+    messagingSenderId: '503341816357',
+    measurementId: 'G-98CPEVZZFF',
+  },
+  production: true
+};
---
+++ lostapp/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'lostapp-2745c',
+    appId: '1:503341816357:web:ed609c20f1d88392c3bdcc',
+    storageBucket: 'lostapp-2745c.appspot.com',
+    apiKey: 'AIzaSyCUKrt60o_JgDXLUeCKylLf7ijzZE2kCMU',
+    authDomain: 'lostapp-2745c.firebaseapp.com',
+    messagingSenderId: '503341816357',
+    measurementId: 'G-98CPEVZZFF',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
